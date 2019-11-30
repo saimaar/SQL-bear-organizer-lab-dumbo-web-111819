2721b46759f21aadba5120b404d0432403f8d085
“Saima <rahman.saima92@gmail.com>
2019-11-29 21:50:07 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 2, "f", "white", "cool", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Rowdy", 3, "f", "white", "cool", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Tabitha", 4, "m", "white", "cool", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 5, "m", "black", "angry", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Melissa", 6, "m", "black", "angry", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Grinch", 7, "m", "brown", "mixed", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Wendy", 8, "f", "black", "mixed", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES(null, 9, "f", "brown", "mixed", TRUE);
